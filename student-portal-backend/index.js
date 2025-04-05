bc5454a8de70ee2674937d74651ce156d6197f9c
Anurag23goel <anurag.go3l07@gmail.com>
2025-04-05 09:27:24 +0530

    changes in index.js

--- student-portal-backend/index.js
+++ student-portal-backend/index.js
@@ -2,46 +2,41 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 require("dotenv").config();
-const { fetchDataByMobile }= require("./service.js");
+const { fetchDataByMobile } = require("./service.js");
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
-// });
-
-
-app.get("/get-student", async (req, res) => {
-    const authHeader = req.headers.authorization;
-    console.log(authHeader);
-    
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        return res.status(400).json({ error: "Mobile number is required" });
-    }
-
-    const mobNo = authHeader.split("Bearer ")[1];
-
-    try {
-        const studentData = await fetchDataByMobile(mobNo);
-        res.json(studentData);
-    } catch (error) {
-        res.status(500).json({ error: "Failed to fetch student data", error });
-    }
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
+app.get("/get-student", async (req, res) => {
+  const authHeader = req.headers.authorization;
+  console.log(authHeader);
 
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(400).json({ error: "Mobile number is required" });
+  }
+
+  const mobNo = authHeader.split("Bearer ")[1];
+
+  try {
+    const studentData = await fetchDataByMobile(mobNo);
+    res.json(studentData);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch student data", error });
+  }
+});
 
 app.get("/api/message", (req, res) => {
-    res.json({ message: "Hello from Express Backend!" });
-  });
-  
+  res.json({ message: "Hello from Express Backend!" });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
